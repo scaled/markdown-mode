f441411e6acff2f6b10ed08214bfa0180f49bbff
Michael Bayne <mdb@fb.com>
2016-08-28 15:51:56 -0700

    Custom refill for list items.

--- src/scala/scaled/markdown/MarkdownMode.scala
+++ src/scala/scaled/markdown/MarkdownMode.scala
@@ -7,6 +7,8 @@ package scaled.markdown
 import scaled._
 import scaled.grammar._
 import scaled.major.TextConfig
+import scaled.Matcher
+import scaled.util.Filler
 
 object MarkdownConfig extends Config.Defs {
   import EditorConfig._
@@ -58,4 +60,35 @@ class MarkdownMode (env :Env) extends GrammarTextMode(env) {
   // override def stylesheets = stylesheetURL("/todo.css") :: super.stylesheets
   override protected def grammars = MarkdownConfig.grammars.get
   override protected def effacers = MarkdownConfig.effacers
+
+  override def refillLinesIn (start :Loc, end :Loc) {
+    // determine whether the first line starts with a list item char (+-#*) followed by space
+    var first = buffer.line(start)
+    var firstStart = first.firstNonWS
+    if (!first.matches(ListItemM, firstStart)) super.refillLinesIn(start, end)
+    else {
+      val r = trimRegion(start, end)
+      val orig = buffer.region(r)
+
+      // add the text to a filler configured with the appropriate width
+      val prefixLen = firstStart+ListItemM.matchLength
+      val filler = new Filler(fillColumn-prefixLen)
+      filler.append(first.view(prefixLen, first.length))
+      orig.drop(1) foreach(filler.append)
+
+      // now prepend the appropriate prefix back onto each filled line and create lines
+      val result = Seq.builder[Line]
+      val filled = filler.filled
+      result += first.view(0, prefixLen) merge Line(filled.head)
+      // subsequent lines get whitespace as a prefix
+      var repeatPre = " " * prefixLen
+      filled.drop(1) foreach { f => result += Line(f.insert(0, repeatPre)) }
+      var flines = result.build()
+
+      if (flines != orig) buffer.replace(r, flines)
+      else window.emitStatus("Region already filled.")
+    }
+  }
+
+  private val ListItemM = Matcher.regexp("""(-|\+|\*|\d+\.) """)
 }
